186a35b9eb058a4d700d6b17d72e4e3b2a67cd59
Lakshmi Narasimha Reddy <lnreddyreddy405@gmail.com>
2025-09-28 17:30:47 +0530

    intial commit

---
+++ authentication_service/src/test/java/com/lnr/authentication_service/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lnr.authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
